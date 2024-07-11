10353285aad96c5018fc9b5f1f39bc182320e1f2
Satish <satishkasani@gmail.com>
2024-07-11 17:16:13 +0530

    test file update

--- src/main/java/com/demo/Test.java
+++ src/main/java/com/demo/Test.java
@@ -3,8 +3,8 @@ package com.demo;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("First program");
+		System.out.println("second line");
 
 	}
 
